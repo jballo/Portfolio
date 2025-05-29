5f73a539fc38b0dd81afec24bf2e688a3ddecca7
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-05-29 14:56:33 -0700

    Feat: Changed tab title from 'work' to 'experience'. Changed content width

--- src/app/(root)/components/EducationWork.tsx
+++ src/app/(root)/components/EducationWork.tsx
@@ -7,12 +7,12 @@ import EducationHistory from "./EducationHistory";
 
 export default function EducationWork() {
     return (<section className="flex flex-col w-full">
-        <Tabs defaultValue="work" className="w-full gap-5">
+        <Tabs defaultValue="experience" className="w-full gap-5">
             <TabsList className="w-full">
-                <TabsTrigger value="work">Work</TabsTrigger>
+                <TabsTrigger value="experience">Experience</TabsTrigger>
                 <TabsTrigger value="education">Education</TabsTrigger>
             </TabsList>
-            <TabsContent value="work">
+            <TabsContent value="experience">
                 <WorkHistory />
             </TabsContent>
             <TabsContent value="education">
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { Sun } from "lucide-react";
 
 
 export default function Header() {
-    return (<header className="w-full max-w-4xl mx-auto sm:px-6 py-6 lg:px-8">
+    return (<header className="w-full max-w-3xl mx-auto sm:px-6 py-6 lg:px-8">
         <nav className="flex flex-row justify-between w-full">
             <ul className="flex flex-row gap-6">
                 <li>
