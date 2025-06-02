ae812770920804efe73aead7d76fdb8327548586
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-06-02 15:40:27 -0700

    Feat: Added skills to home page
    Note: Added Tooltip for skills

---
+++ src/app/(root)/components/Skills.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/atoms/tooltip";
+import Firebase from "@/icons/Firebase";
+import HuggingFace from "@/icons/Huggingface";
+import Langchain from "@/icons/Langchain";
+import Nextjs from "@/icons/Nextjs";
+import PostgreSQL from "@/icons/Postgresql";
+import React from "@/icons/React";
+import Stripe from "@/icons/Stripe";
+import Supabase from "@/icons/Supabase";
+import TailwindCSS from "@/icons/Tailwindcss";
+import TensorFlow from "@/icons/Tensorflow";
+import Typescript from "@/icons/Typescripticon";
+
+
+export default function Skills() {
+    return (<section className="flex flex-col w-full gap-4">
+        <h2 className="text-3xl">Skills</h2>
+        <div className="flex flex-row flex-wrap justify-start sm:justify-evenly gap-4">
+            <Tooltip>
+                <TooltipTrigger>
+                    <Nextjs width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Nextjs</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <Typescript width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Typescript</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <React width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>React</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <TailwindCSS width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>TailwindCSS</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <Firebase width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Firebase</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <Supabase width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Supabase</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <PostgreSQL width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>PostgreSQL</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <TensorFlow width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Tensorflow</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <Stripe width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Stripe</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <Langchain width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Langchain</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger>
+                    <HuggingFace width={35} height={35} />
+                </TooltipTrigger>
+                <TooltipContent>Huggingface</TooltipContent>
+            </Tooltip>
+        </div>
+    </section>)
+}
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,6 +2,7 @@ import EducationWork from "./components/EducationWork";
 import FeaturedProjects from "./components/FeaturedProjects";
 import Hero from "./components/Hero";
 import RecentBlogs from "./components/RecentBlogs";
+import Skills from "./components/Skills";
 
 
 export default function Home() {
@@ -9,6 +10,8 @@ export default function Home() {
     <div className="flex flex-col w-full max-w-3xl mx-auto py-6 px-4 sm:px-6 lg:px-8 gap-12">
       {/* Hero Section */}
       <Hero />
+      {/* Skills Section */}
+      <Skills />
       {/* Work + Education Section */}
       <EducationWork />
       {/* Featured Projects Section */}
